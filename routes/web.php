d593b2980bef8f20507c08ec7448dc39867f4559
aliyaputriramadhani <aliya.putriramadhani01@gmail.com>
2026-04-08 00:14:28 +0700

    update view login

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/login', [LoginController::class, 'tampilkan']);
 Route::post('/login', [LoginController::class, 'prosesLogin']);
 
-
 Route::get('/', [HomeController::class, 'tampilkan']);
 Route::get('/home', [HomeController::class, 'tampilkan']);
-
-
-Route::view('/login', 'login');
